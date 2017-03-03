ccc18e356a4e5dfdce2bbf51a44639c726e99eed
IreneKhramova <Irinka379@yandex.ru>
2017-03-03 18:21:07 +0300

    Initial commit

---
+++ letters/main.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include<fstream>
+#include <string>
+#include<locale>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    const int N=33;
+    ifstream fin("in2.txt");
+    setlocale(LC_ALL,"rus");
+    if(!fin.is_open())
+        cout << "File didn't open\n";
+    else
+    {
+        const double statistics[N]={0.07998, 0.01592, 0.04533, 0.01687, 0.02977, 0.08483, 0.00013,
+                              0.0094, 0.01641, 0.07367, 0.01208, 0.03486, 0.04343, 0.03203, 0.067,
+                              0.10983, 0.02804, 0.04746, 0.05473, 0.06318, 0.02615, 0.00267, 0.00966,
+                              0.00486, 0.0145, 0.00718, 0.00361, 0.00037, 0.01898, 0.01735, 0.00331,
+                              0.00639, 0.02001};
+
+        int letter[2][N];
+        for(int i=0; i<N; i++)
+        {
+            letter[1][i]=0;
+             letter[0][i]=224+i;
+        }
+        letter[0][N-1]=184;//ё
+
+        int cnt=0;
+        int c;
+
+        while((c = fin.get()) != EOF)
+        {
+            //cout << (char)c << " " << c << endl;
+            char t=tolower(c);//(char)c;
+            int code=(int)tolower(c);
+            //cout << t << " " << code << endl;
+            if(((code >= 224) && (code <= 255)) || (code==184))
+            {
+                //считаем, сколько раз встретилась буква
+                //cout << t << " " << code << endl;
+                if(code==184)//ё
+                    letter[1][N-1]++;
+                else
+                    letter[1][code-224]++;
+                //кол-во букв в тексте
+                cnt++;
+            }
+        }
+
+        //cout << cnt << endl;
+        double freq[N];//частотность
+        for(int i=0; i<N; i++)
+        {
+            freq[i]=(double)letter[1][i] / cnt;
+        }
+
+        ofstream fout("out.txt");
+        fout << "#     frequency in text   statistic\n";
+        for(int i=0; i<N; i++)
+        {
+            fout << i+1 << ") ";
+            if(i<6)
+                fout << (char)letter[0][i] << " \t" << setw(13) << freq[i] << "  \t" << setw(9) << statistics[i] << endl;
+            else if(i==6)
+                fout << (char)letter[0][N-1] << " \t" << setw(13) << freq[N-1] << "  \t" << setw(9) << statistics[N-1] << endl;
+            else
+                fout << (char)letter[0][i-1] << " \t" << setw(13) << freq[i-1] << "  \t" << setw(9) << statistics[i-1] << endl;
+
+        }
+        fout.close();
+    }
+    return 0;
+}
---
+++ matr/main.cpp
@@ -0,0 +1,262 @@
+#include <iostream>
+#include<fstream>
+#include<cmath>
+
+using namespace std;
+
+void getMatr(double **mas, double **p, int i, int j, int m);
+double determinant(double **mas, int m);
+
+int main()
+{
+    ifstream aIn("a.in.txt");
+    ifstream bIn("b.in.txt");
+    if(!aIn.is_open() || !bIn.is_open())
+        cout << "Files didn't open\n";
+    else
+    {
+        int N;
+        aIn >> N;
+        bool isCorrect=1;
+
+        double **a = new double*[N];
+        for(int i=0; i<N; i++)
+            a[i] = new double[N];
+
+        for(int i=0; i<N; i++)
+            for(int j=0; j<N; j++)
+            {
+                int t;
+                aIn >> t;
+                if(isdigit(t))
+                    a[i][j]=t;
+                else
+                {
+                    ofstream fout("x.out.txt");
+                    fout << "Матрица задана неправильно" << endl;
+                    fout.close();
+                    isCorrect=0;
+                    break;
+                }
+            }
+
+
+        aIn.close();
+        if(isCorrect)
+        {
+            double det=determinant(a, N);//определитель
+            double eps=1e-7;
+            if(abs(det)<eps)//==0
+            {
+                ofstream fout("x.out.txt");
+                //нет решений или их бесконечно много
+                fout << "Определитель матрицы равен нулю, значит матрица необратима." << endl;
+                fout.close();
+            }
+            else
+            {
+                double *b = new double[N];
+                double *x = new double[N];
+
+                for(int j=0; j<N; j++)
+                {
+                    x[j]=0;
+
+                    int t;
+                    bIn >> t;
+                    if(isdigit(t))
+                        b[j]=t;
+                    else
+                    {
+                        ofstream fout("x.out.txt");
+                        fout << "Матрица задана неправильно" << endl;
+                        fout.close();
+                        isCorrect=0;
+                        break;
+                    }
+
+                }
+                if(isCorrect)
+                {
+                    double **aTemp = new double*[N];
+                    for(int i=0; i<N; i++)
+                        aTemp[i] = new double[N];
+
+                    for(int i=0; i<N; i++)
+                        for(int j=0; j<N; j++)
+                            aTemp[i][j]=a[i][j];
+
+                    //обратная матрица
+                    double **aInvert = new double*[N];
+                    for(int i=0; i<N; i++)
+                        aInvert[i] = new double[N];
+
+                    for(int i=0; i<N; i++)
+                        for(int j=0; j<N; j++) // E
+                            if(i==j)
+                                aInvert[i][j]=1;
+                            else
+                                aInvert[i][j]=0;
+
+///////////////////////обратная матрица - метод Гаусса///////////////////
+
+
+                    double temp;
+                    //прямой ход
+                    for (int k = 0; k < N; k++)
+                    {
+                        temp = aTemp[k][k];
+
+                        //получаем единицу в a[k][k]
+                        for (int j = 0; j < N; j++)
+                        {
+                            aTemp[k][j] /= temp;
+                            aInvert[k][j] /= temp;
+                        }
+
+                        for (int i = k + 1; i < N; i++)
+                        {
+                            temp = aTemp[i][k];
+                            /*получаем нули под единицей -
+                            вычитаем из i-ой строки k-ую, умноженную на коэф a[i][k] */
+                            for (int j = 0; j < N; j++)
+                            {
+                                aTemp[i][j] -= aTemp[k][j] * temp;
+                                aInvert[i][j] -= aInvert[k][j] * temp;
+                            }
+                        }
+
+            /*for(int i=0;i<N;i++)
+            {
+                for(int j=0;j<N;j++)
+                    cout << aTemp[i][j] << "  ";
+                cout << endl;
+            }
+            cout << endl;*/
+                }
+
+                    //обратный ход
+                    for (int k = N - 1; k > 0; k--)
+                    {
+                        for (int i = k - 1; i >= 0; i--)
+                        {
+                            temp = aTemp[i][k];
+                            //получаем нули над единицами
+                            for (int j = 0; j < N; j++)
+                            {
+                                aTemp[i][j] -= aTemp[k][j] * temp;
+                                aInvert[i][j] -= aInvert[k][j] * temp;
+                            }
+                        }
+                    }
+
+//////////////////////решение/////////////////////////////////
+
+                    //x = aInvert * b
+                    for(int i=0; i<N; i++)
+                        for(int j=0; j<N; j++)
+                            x[i] += aInvert[i][j] * b[j];
+
+                    ofstream fout("x.out.txt");
+                    for(int i=0; i<N; i++)
+                        fout << x[i] << endl;
+
+
+//////////////////////проверка/////////////////////////////
+
+                    //A * x = b
+                    fout << "\nПроверка:\nA*x\tb\n";
+                    double t=0;
+                    for(int i=0; i<N; i++)
+                    {
+                        t=0;
+                        for(int j=0; j<N; j++)
+                            t += a[i][j] * x[j];
+                        fout << t << "\t" << b[i] << "\n";
+                    }
+
+
+ /////////////////////////////////////////////////////////
+
+                    fout.close();
+
+                    delete [] b;
+                    delete [] x;
+
+                    for(int i=0; i<N; i++)
+                        delete [] aTemp[i];
+                    delete [] aTemp;
+
+                    for(int i=0; i<N; i++)
+                        delete [] aInvert[i];
+                    delete [] aInvert;
+                }
+            }
+            bIn.close();
+
+            for(int i=0; i<N; i++)
+                delete [] a[i];
+            delete [] a;
+        }
+    }
+    return 0;
+}
+
+
+// Получение матрицы без i-й строки и j-го столбца
+void getMatr(double **mas, double **p, int i, int j, int m)
+{
+    int di, dj;
+    di = 0;
+    for (int ki = 0; ki<m - 1; ki++)
+    {
+        // проверка индекса строки
+        if (ki == i)
+            di = 1;
+        dj = 0;
+        for (int kj = 0; kj<m - 1; kj++)
+        {
+            // проверка индекса столбца
+            if (kj == j)
+                dj = 1;
+            p[ki][kj] = mas[ki + di][kj + dj];
+        }
+    }
+}
+// Рекурсивное вычисление определителя
+double determinant(double **mas, int m)
+{
+    double d=0;
+    double **p;
+    p = new double*[m];
+    for (int i = 0; i<m; i++)
+        p[i] = new double[m];
+    int k = 1; //(-1) в степени i
+    if (m<1)
+        cout << "Определитель вычислить невозможно!";
+    if (m == 1)
+    {
+        d = mas[0][0];
+        return(d);
+    }
+    if (m == 2)
+    {
+        d = mas[0][0] * mas[1][1] - (mas[1][0] * mas[0][1]);
+        return(d);
+    }
+    if (m>2)
+    {
+        for (int i = 0; i<m; i++)
+        {
+            getMatr(mas, p, i, 0, m);
+            d += k * mas[i][0] * determinant(p, m-1);
+            k = -k;
+        }
+    }
+
+    for(int i=0; i<m; i++)
+            delete [] p[i];
+        delete [] p;
+
+    return(d);
+}
